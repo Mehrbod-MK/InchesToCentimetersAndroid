2a626a6ce58fa0cbdb5c8ee4af85ba27879761c8
Asus <mehrbodmollakazemi81@gmail.com>
2025-07-25 17:41:46 +0330

    v0.1.0.0 - Finished logic design.

--- app/src/main/java/com/mehrbodmk/inchestocentimeters/MainActivity.kt
+++ app/src/main/java/com/mehrbodmk/inchestocentimeters/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.mehrbodmk.inchestocentimeters
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editTextInches: EditText
+    private lateinit var buttonConvert: Button
+    private lateinit var textViewResult: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +24,21 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        editTextInches = findViewById(R.id.editTextNumberDecimal)
+        buttonConvert = findViewById(R.id.buttonConvert)
+        textViewResult = findViewById(R.id.textViewResult)
+
+        buttonConvert.setOnClickListener {
+            val inputNumberText = editTextInches.text.toString().toDoubleOrNull()
+            if (inputNumberText?.isNaN() == false) {
+                textViewResult.text = getString(R.string.text_ResultCM, (inputNumberText * 2.54).toString())
+            }
+            else
+            {
+                Toast.makeText(this, R.string.toast_NotAValidDecimal, Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
